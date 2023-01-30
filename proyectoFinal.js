8b534ce930471ace943f46c56b578d71ae81ed16
joel <joelmontiel2014jm@gmail.com>
2023-01-30 00:08:13 -0300

    mi primer commit

---
+++ proyectoFinal.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("button")
+let fecha = document.getElementById("fecha")
+let institucion = document.getElementById("institucion")
+let titulo = document.getElementById("titulo")
+
+button.addEventListener("click", function(){
+   fecha.innerText = "2008-2014"
+  titulo.innerText = "Bachiller en ciencias economia"
+  institucion.innerText = "Media 9/Fragata sarmiento"
+})
